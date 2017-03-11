3ad1e88f7687f82e587d6c38c7a2213b5dde60c3
OliverCarswell <visiongamingjake@hotmail.com>
2017-03-11 14:53:13 +0100

    Removal of outdated plugin.

--- clockwork/plugins/recipes/plugin/entities/entities/cw_workbench/shared.lua
+++
@@ -1,9 +0,0 @@
-DEFINE_BASECLASS("base_gmodentity");
-
-ENT.Type = "anim";
-ENT.Author = "Duck";
-ENT.PrintName = "Workbench";
-ENT.Spawnable = false;
-ENT.AdminSpawnable = false;
-ENT.UsableInVehicle = true;
-ENT.PhysgunDisabled = true;
