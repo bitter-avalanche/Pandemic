e30e2b7398d895a8775f03820f16f292e7ccf68f
Zorxas <maxmyler103@gmail.com>
2017-03-19 18:06:43 -0400

    le squashie fix thing

--- cwphasefour/schema/items/sh_parts_pistolcasing.lua
+++ cwphasefour/schema/items/sh_parts_pistolcasing.lua
@@ -1,5 +1,5 @@
 --[[
-	© 2013 CloudSixteen.com do not share, re-distribute or modify
+	Â© 2013 CloudSixteen.com do not share, re-distribute or modify
 	without permission of its author (kurozael@gmail.com).
 --]]
 
@@ -15,8 +15,11 @@ ITEM.business = true;
 ITEM.category = "Gunparts";
 ITEM.description = "A tattered casing designed to manufacture pistols. It's not the best, but it'll do.";
 ITEM.spawnType = "gunparts";
+ITEM.spawnValue = 35;
+
 
 -- Called when a player drops the item.
 function ITEM:OnDrop(player, position) end;
 
 ITEM:Register();
+
