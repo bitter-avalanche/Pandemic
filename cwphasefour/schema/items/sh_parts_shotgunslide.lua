ea9a7a700f967b6337764162ddf308644a6d8f8d
WrongEnd <WrongEnd@users.noreply.github.com>
2017-03-20 17:56:24 -0500

    Adds in Spawn Values for Everything with a Spawntype (#35)
    * Adds spawn values for everything with a spawntype
    
    * Updates values

--- cwphasefour/schema/items/sh_parts_shotgunslide.lua
+++ cwphasefour/schema/items/sh_parts_shotgunslide.lua
@@ -15,6 +15,7 @@ ITEM.business = true;
 ITEM.category = "Gun parts";
 ITEM.description = "A usable shotgun slide. It can be used in gun crafting.";
 ITEM.spawnType = "gunparts";
+ITEM.spawnValue = 22;
 
 -- Called when a player drops the item.
 function ITEM:OnDrop(player, position) end;
