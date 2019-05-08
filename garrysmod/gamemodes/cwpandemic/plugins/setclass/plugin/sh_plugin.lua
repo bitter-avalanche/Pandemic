58c9e3f60897137d7dc8777c943a6a02a5401b2f
The Lion <visiongamingjake@hotmail.com>
2019-05-08 15:36:38 +0200

    Ports over setclass.

---
+++ garrysmod/gamemodes/cwpandemic/plugins/setclass/plugin/sh_plugin.lua
@@ -0,0 +1,9 @@
+--[[
+This plugin was initially created for Bitter Avalanche.
+
+Use is public, but these credits must not be removed at any date or time.
+
+For this plugin, additional credits are applied, found in plugin.ini
+--]]
+
+local PLUGIN = PLUGIN;
